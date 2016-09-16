874170c8b996cb1136b983b333d5577a02351de9
Wilker <364105996@qq.com>
2016-09-16 18:30:50 +0800

    - 添加寻路 - Enemy 添加移动到 Player

---
+++ Assets/Scripts/Enemy_target.cs
@@ -0,0 +1,65 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.Networking;
+
+public class Enemy_target : NetworkBehaviour
+{
+    private NavMeshAgent agent;
+    private Transform myTransform;
+    private Transform targetTransform;
+    private LayerMask raycastLayer;
+    private float radius = 1000.0f;
+
+    void Start()
+    {
+        agent = GetComponent<NavMeshAgent>();
+        myTransform = transform;
+        raycastLayer = 1 << LayerMask.NameToLayer("Player");
+    }
+
+    public void FixedUpdate()
+    {
+        SearchForTarget();
+        MoveToTarget();
+    }
+
+    void SearchForTarget()
+    {
+        if (!isServer)
+            return;
+
+
+        if (targetTransform == null)
+        {
+            Collider[] hitColiders = Physics.OverlapSphere(myTransform.position, radius, raycastLayer);
+
+            if (hitColiders.Length > 0)
+            {
+                int randomint = Random.Range(0, hitColiders.Length);
+                targetTransform = hitColiders[randomint].transform;
+            }
+        }
+
+        if (targetTransform != null && targetTransform.GetComponent<CapsuleCollider>().enabled == false)
+        {
+            targetTransform = null;
+            Debug.LogFormat("--- target name:{0}", targetTransform.name);
+        }
+    }
+
+    void MoveToTarget()
+    {
+        if (!isServer)
+            return;
+
+        if (targetTransform != null)
+        {
+            SetNavDestination(targetTransform);
+        }
+    }
+
+    void SetNavDestination(Transform dest)
+    {
+        agent.SetDestination(dest.position);
+    }
+}
