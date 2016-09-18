234ab2c7aa7a04d5e854b1347800e0aeca0fc37d
yangxuan <yx0588@175game.com>
2016-09-18 18:07:37 +0800

    - 添加 NetworkProximityCheckerCustom

---
+++ Assets/testTargetSync/MoveCtrl.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.Networking;
+
+public class MoveCtrl : NetworkBehaviour {
+
+    Transform myTransform;
+
+	// Use this for initialization
+	void Start () {
+        myTransform = transform;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (!isLocalPlayer)
+            return;
+
+        float x = Input.GetAxis("Horizontal") * Time.deltaTime * 150.0f;
+        float z = Input.GetAxis("Vertical") * Time.deltaTime * 3.0f;
+
+        transform.Rotate(0, x, 0);
+        transform.Translate(0, 0, z);
+    }
+}
